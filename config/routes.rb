9c4337a2590f5f190f05c866ff6e78f122c91afc
Jalena Taylor <jalena.penaligon@gmail.com>
2019-11-01 13:34:15 -0600

    Adds README

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/users', to: 'users#index'
-      post '/users', to: 'users#create'
+      resources :users, only: [:index, :create]
       post '/users/water', to: 'water_logs#create'
     end
   end
